8948ec185274ac10895c6ee02395a226c44e971d
Kyuuhachi <caagr98@gmail.com>
2023-03-27 13:58:18 +0200

    Misc stuff

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::borrow::Cow;
 use std::ffi::OsString;
 use std::io::{Read, Write, Seek, SeekFrom};
 use std::os::windows::ffi::OsStringExt;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 use windows::Win32::System::LibraryLoader::GetModuleFileNameW;
 use windows::core::HRESULT;
@@ -15,7 +15,7 @@ use windows::Win32::Foundation::{HINSTANCE, BOOL, TRUE, FALSE};
 pub extern "system" fn DllMain(_dll_module: HINSTANCE, reason: u32, _reserved: *const ()) -> BOOL {
 	if reason != 1 /* DLL_PROCESS_ATTACH */ { return TRUE }
 
-	// println!("SoraData: init for {}", *NAME);
+	println!("SoraData: init for {}", *NAME);
 
 	match init() {
 		Ok(()) => TRUE,
@@ -35,7 +35,7 @@ pub extern "system" fn DirectXFileCreate(_dxfile: *const *const ()) -> HRESULT {
 }
 
 lazy_static::lazy_static! {
-	static ref PATH: PathBuf = {
+	static ref EXE_PATH: PathBuf = {
 		let mut path = [0; 260];
 		unsafe {
 			GetModuleFileNameW(HINSTANCE(0), &mut path);
@@ -43,11 +43,12 @@ lazy_static::lazy_static! {
 		let path = &path[..path.iter().position(|a| *a == 0).expect("has nul")];
 		PathBuf::from(OsString::from_wide(path))
 	};
+	static ref GAME_DIR: &'static Path = {
+		EXE_PATH.parent().unwrap()
+	};
 	static ref NAME: &'static str = {
-		let path = PATH.to_str().expect("is utf8");
-		let name = path.rsplit('\\').next().expect("split is nonempty");
-		let name = Box::leak(name.to_lowercase().into_boxed_str());
-		name.strip_suffix(".exe").expect("ends with .exe")
+		let name = EXE_PATH.file_stem().unwrap().to_str().unwrap();
+		Box::leak(name.to_lowercase().into_boxed_str())
 	};
 	static ref ADDRS: Addrs = Addrs::get(*NAME).expect("exe not supported");
 }
@@ -118,7 +119,7 @@ unsafe fn tour(a: *const (), b: *const ()) -> anyhow::Result<()> {
 // read_from_dat has an unusual callconv that is like fastcall, but caller-cleanup.
 #[naked]
 unsafe extern "fastcall" fn read_from_dat_wrap(buf: *mut u8, archive_no: u32, offset: usize, length: usize) -> bool {
-	std::arch::asm!{
+	std::arch::asm! {
 		"push [esp+8]",
 		"push [esp+8]",
 		"call {read_from_dat}",
@@ -140,48 +141,46 @@ extern "fastcall" fn read_from_dat(buf: *mut u8, archive_no: u32, offset: usize,
 
 fn do_read(buf: *mut u8, archive_no: u32, offset: usize, length: usize) -> anyhow::Result<()> {
 	let index = ADDRS.dir_entries()[archive_no as usize].iter()
-		.position(|e| e.offset == offset && e.asize != 0);
-	let Some(index) = index else {
-		anyhow::bail!("no match");
-	};
+		.position(|e| e.offset == offset && e.csize == length);
+	if let Some(index) = index {
+		let raw_name = &ADDRS.dir_entries()[archive_no as usize][index].name();
+		let name = format!("data/ED6_DT{archive_no:02x}/{}", normalize_name(raw_name));
 
-	let name = normalize_name(&ADDRS.dir_entries()[archive_no as usize][index].name());
-	let gamedir = PATH.parent().unwrap();
-	let file = gamedir.join(format!("data/ED6_DT{archive_no:02x}/{name}"));
-	println!("SoraData: {} ({length}B)", file.display());
-	if file.exists() {
-		println!("   exists");
-		if is_raw(&name) {
-			let data = std::fs::read(file)?;
-			let buf = unsafe { std::slice::from_raw_parts_mut(buf, 0x600000) };
-			fake_compress(&mut std::io::Cursor::new(buf), &data)?;
-		} else {
-			let mut f = std::fs::File::open(file)?;
-			let buf = unsafe { std::slice::from_raw_parts_mut(buf, length) };
-			f.read_exact(buf)?;
+		println!("SoraData: {name}");
+
+		let path = GAME_DIR.join(&name);
+		if path.exists() {
+			println!("   exists, redirecting");
+			if is_raw(&name) {
+				let mut f = std::fs::File::open(path)?;
+				let buf = unsafe { std::slice::from_raw_parts_mut(buf, length) };
+				f.read_exact(buf)?;
+			} else {
+				let data = std::fs::read(path)?;
+				let buf = unsafe { std::slice::from_raw_parts_mut(buf, 0x600000) };
+				fake_compress(&mut std::io::Cursor::new(buf), &data)?;
+			}
+			return Ok(())
 		}
-		Ok(())
-	} else {
-		let dirfile = gamedir.join(format!("ED6_DT{archive_no:02x}.DAT"));
-		let mut f = std::fs::File::open(dirfile)?;
-		f.seek(SeekFrom::Start(offset as u64))?;
-		let buf = unsafe { std::slice::from_raw_parts_mut(buf, length) };
-		f.read_exact(buf)?;
-		Ok(())
 	}
+
+	let datfile = GAME_DIR.join(format!("ED6_DT{archive_no:02x}.DAT"));
+	let mut f = std::fs::File::open(datfile)?;
+	f.seek(SeekFrom::Start(offset as u64))?;
+	let buf = unsafe { std::slice::from_raw_parts_mut(buf, length) };
+	f.read_exact(buf)?;
+	Ok(())
 }
 
 fn fake_compress(buf: &mut impl Write, data: &[u8]) -> anyhow::Result<()> {
-	let mut chunks = data.chunks((1<<16)-1).peekable();
+	let mut chunks = data.chunks(0x1FFF).peekable();
+	buf.write_all(&u16::to_le_bytes(2))?;
+	buf.write_all(&[chunks.peek().is_some().into()])?;
 	while let Some(chunk) = chunks.next() {
-		buf.write_all(&u16::to_le_bytes((chunk.len() as u16).wrapping_add(2)))?;
-		for sub in chunk.chunks((1<<13)-1) {
-			if sub.len() >= 1<<5 {
-				buf.write_all(&[1<<5 | (sub.len() >> 8) as u8])?;
-			}
-			buf.write_all(&[sub.len() as u8])?;
-			buf.write_all(sub)?;
-		}
+		let len = chunk.len() as u16;
+		buf.write_all(&u16::to_le_bytes(len + 4))?;
+		buf.write_all(&u16::to_be_bytes(len | 0x2000))?; 
+		buf.write_all(chunk)?;
 		buf.write_all(&[chunks.peek().is_some().into()])?;
 	}
 	Ok(())
