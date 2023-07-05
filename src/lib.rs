ed6e49ec183d4adb8c3ef32f6da04c6d0bf1f3fa
Kyuuhachi <caagr98@gmail.com>
2023-07-05 14:45:27 +0200

    Cleanup and doc

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,7 @@ pub extern "system" fn DirectXFileCreate(_dxfile: *const *const ()) -> HRESULT {
 }
 
 lazy_static::lazy_static! {
+	/// Path to the main executable, generally named `ed6_win_something.exe`.
 	static ref EXE_PATH: PathBuf = {
 		let mut path = [0; 260];
 		let n = unsafe {
@@ -54,6 +55,10 @@ lazy_static::lazy_static! {
 		let path = OsString::from_wide(&path[..n as usize]);
 		PathBuf::from(path)
 	};
+	/// Path to the game directory, where all game files are located.
+	static ref GAME_DIR: &'static Path = EXE_PATH.parent().unwrap();
+	/// Path to LB-DIR data directory, where LB-DIR reads the files from.
+	static ref DATA_DIR: PathBuf = GAME_DIR.join("data");
 }
 
 mod hooks {
@@ -64,6 +69,7 @@ mod hooks {
 	}
 }
 
+/// Initializes the hooks.
 fn init() -> anyhow::Result<()> {
 	unsafe {
 		hooks::read_from_file.initialize(std::mem::transmute(sigscan! {
@@ -83,36 +89,44 @@ fn init() -> anyhow::Result<()> {
 	Ok(())
 }
 
-fn read_dir_files() {
-	unsafe {
-		hooks::read_dir_files.call();
-	}
-
-	show_error(do_load_dir());
-}
-
+/// Called by the game to read from any file into memory.
+///
+/// This is called both for .dat and other files
 fn read_from_file(handle: *const HANDLE, buf: *mut u8, len: usize) -> usize {
+	// Get path to file
 	let mut path = [0; 260];
 	let n = unsafe {
 		GetFinalPathNameByHandleW(*handle, &mut path, FILE_NAME(0))
 	} as usize;
-	let path = OsString::from_wide(&path[..n]);
-	let path = PathBuf::from(path);
+	let path = PathBuf::from(OsString::from_wide(&path[..n]));
 
-	if let Some(nr) = parse_archive_nr(&path) {
+	// If the pathname refers to a .dat file, extract its number
+	let dirnr = try {
+		let name = path.file_name()?.to_str()?;
+		let name = name.strip_prefix("ED6_DT")?.strip_suffix(".dat")?;
+		usize::from_str_radix(name, 16).ok()?
+	};
+
+	if let Some(dirnr) = dirnr {
+		// If it is a dir file, we still don't know *which* file is being loaded.
+		// All we have is the file position, which is set by a different function.
+		// So we extract the offset and find the file with the corresponding offset from the .dat file.
 		let pos = unsafe {
 			SetFilePointer(*handle, 0, None, SET_FILE_POINTER_MOVE_METHOD(1))
 		} as usize;
 
 		let dirs = DIRS.lock().unwrap();
-		let entry = dirs.entries()[nr].iter()
+		let entry = dirs.entries()[dirnr].iter()
 			.enumerate()
 			.find(|(_, e)| e.offset == pos && e.csize == len);
 
-		if let Some((_, entry)) = entry {
-			let buf = unsafe { std::slice::from_raw_parts_mut(buf, 0x600000) };
-			if let Some(v) = do_read(nr, entry, buf) {
-				return v
+		if let Some((filenr, entry)) = entry {
+			let fileid = ((dirnr << 16) | filenr) as u32;
+			// We only have an unstructured buffer to write to.
+			if let Some(v) = get_redirect_file(fileid, entry) {
+				let buf = unsafe { std::slice::from_raw_parts_mut(buf, v.len()) };
+				buf.copy_from_slice(&v);
+				return v.len()
 			}
 		}
 	}
@@ -122,54 +136,74 @@ fn read_from_file(handle: *const HANDLE, buf: *mut u8, len: usize) -> usize {
 	}
 }
 
-fn msgbox(title: &str, body: &str, style: u32) -> u32 {
-	let mut title = OsString::from(title).encode_wide().collect::<Vec<_>>();
-	let mut body = OsString::from(body).encode_wide().collect::<Vec<_>>();
-	title.push(0);
-	body.push(0);
+/// Reads the file to be redirected to, if any.
+///
+/// Allocating memory here is not strictly necessary, but it makes the code much nicer.
+fn get_redirect_file(fileid: u32, entry: &Entry) -> Option<Vec<u8>> {
+	let dirnr = fileid >> 16;
+	let path = if let Some(path) = path_of(entry) {
+		Some(DATA_DIR.join(format!("ED6_DT{dirnr:02X}")).join(path))
+	} else {
+		Some(DATA_DIR.join(format!("ED6_DT{dirnr:02X}/{}", normalize_name(&entry.name()))))
+			.filter(|a| a.exists())
+	};
+
+	if let Some(path) = path {
+		show_error(c!(read_file(&path)?, "failed to read {}", rel(&path).display()))
+	} else {
+		None
+	}
+}
+
+/// Reads a file into memory, compressing it if necessary.
+///
+/// Most files in the dat files are compressed, but this is inconvenient for users so LB-ARK handles that implicitly.
+fn read_file(path: &Path) -> anyhow::Result<Vec<u8>> {
+	let data = std::fs::read(path)?;
+	let ext: Option<_> = try { path.extension()?.to_str()?.to_lowercase() };
+	let is_raw = ext.map_or(false, |e| e == "_ds" || e == "wav");
+	if is_raw {
+		Ok(data)
+	} else {
+		Ok(fake_compress(&data))
+	}
+}
+
+/// "Compress" data by inserting "raw data" instructions as necessary.
+fn fake_compress(data: &[u8]) -> Vec<u8> {
+	let mut chunks = data.chunks(0x1FFF).peekable();
+	let mut out = Vec::with_capacity(3 + data.len() + chunks.clone().len() * 5);
+
+	// include an empty chunk, because otherwise it'll just read uninitialized data
+	out.extend(&u16::to_le_bytes(2)); // chunk size
+	out.push(chunks.peek().is_some().into()); // has next chunk?
+
+	while let Some(chunk) = chunks.next() {
+		let len = chunk.len() as u16;
+		out.extend(&u16::to_le_bytes(len + 4)); // chunk size
+		out.extend(&u16::to_be_bytes(len | 0x2000)); // compression flags: N bits of raw data
+		out.extend(chunk); // data
+		out.push(chunks.peek().is_some().into()); // has next chunk?
+	}
+
+	out
+}
+
+
+/// Called by the game at startup, to load the .dir files into memory.
+///
+/// This hook additionally loads `$DATA_DIR/ED6_DT??/*.dir`.
+fn read_dir_files() {
 	unsafe {
-		MessageBoxW(
-			None,
-			PCWSTR::from_raw(body.as_ptr()),
-			PCWSTR::from_raw(title.as_ptr()),
-			MESSAGEBOX_STYLE(style)
-		).0 as u32
+		hooks::read_dir_files.call();
 	}
-}
 
-fn show_error<T>(a: anyhow::Result<T>) -> Option<T> {
-	match a {
-		Ok(v) => Some(v),
-		Err(e) => {
-			let s = format!("{e:#}");
-			println!("{:?}", e.context("LB-ARK error"));
-			msgbox("LB-ARK error", &s, 0x10);
-			None
-		}
-	}
-}
-
-fn data_dir(nr: usize) -> PathBuf {
-	EXE_PATH.parent().unwrap().join(format!("data\\ED6_DT{nr:02X}"))
-}
-
-fn rel(path: &Path) -> &Path {
-	path.strip_prefix(EXE_PATH.parent().unwrap()).unwrap()
-}
-
-fn parse_archive_nr(path: &Path) -> Option<usize> {
-	let name = path.file_name()?.to_str()?;
-	let name = name.strip_prefix("ED6_DT")?.strip_suffix(".dat")?;
-	usize::from_str_radix(name, 16).ok()
-}
-
-macro c($e:expr, $($a:tt)*) {
-	<anyhow::Result<_> as anyhow::Context<_, _>>::with_context(try { $e }, || format!($($a)*))
+	show_error(c!(do_load_dir(), "failed to load dir files"));
 }
 
 fn do_load_dir() -> anyhow::Result<()> {
 	for nr in 0..64 {
-		let dir = data_dir(nr);
+		let dir = DATA_DIR.join(format!("ED6_DT{nr:02X}"));
 		if dir.is_dir() {
 			for f in dir.read_dir()? {
 				let path = f?.path();
@@ -239,53 +273,6 @@ fn path_of(e: &Entry) -> Option<&str> {
 	}
 }
 
-fn do_read(nr: usize, entry: &Entry, buf: &mut [u8]) -> Option<usize> {
-	if let Some(path) = path_of(entry) {
-		let path = data_dir(nr).join(path);
-		Some(read_file(&path, buf))
-	} else {
-		let path = data_dir(nr).join(normalize_name(&entry.name()));
-		if path.exists() {
-			Some(read_file(&path, buf))
-		} else {
-			None
-		}
-	}
-}
-
-fn read_file(path: &Path, buf: &mut [u8]) -> usize {
-	let data = show_error(c!(std::fs::read(path)?, "failed to read {}", rel(path).display()))
-		.unwrap_or(Vec::new()); // Give an empty file. Will probably segfault.
-	let ext: Option<_> = try { path.extension()?.to_str()?.to_lowercase() };
-	let is_raw = ext.map_or(false, |e| e == "_ds" || e == "wav");
-	if is_raw {
-		buf[..data.len()].copy_from_slice(&data);
-		data.len()
-	} else {
-		fake_compress(buf, &data)
-	}
-}
-
-fn fake_compress(buf: &mut [u8], data: &[u8]) -> usize {
-	let mut i = 0;
-	let mut put = |y: &[u8]| {
-		buf[i..][..y.len()].copy_from_slice(y);
-		i += y.len();
-	};
-	let mut chunks = data.chunks(0x1FFF).peekable();
-	// include an empty chunk, because otherwise it'll just read uninitialized data
-	put(&u16::to_le_bytes(2));
-	put(&[chunks.peek().is_some().into()]);
-	while let Some(chunk) = chunks.next() {
-		let len = chunk.len() as u16;
-		put(&u16::to_le_bytes(len + 4));
-		put(&u16::to_be_bytes(len | 0x2000));
-		put(chunk);
-		put(&[chunks.peek().is_some().into()]);
-	}
-	i
-}
-
 pub fn normalize_name(name: &str) -> String {
 	let name = name.to_lowercase();
 	if let Some((name, ext)) = name.split_once('.') {
@@ -304,4 +291,42 @@ pub fn unnormalize_name(name: &str) -> Option<[u8; 12]> {
 	o[..name.len()].copy_from_slice(name.as_bytes());
 	o[9..][..ext.len()].copy_from_slice(ext.as_bytes());
 	Some(o)
+}
+
+fn msgbox(title: &str, body: &str, style: u32) -> u32 {
+	let mut title = OsString::from(title).encode_wide().collect::<Vec<_>>();
+	let mut body = OsString::from(body).encode_wide().collect::<Vec<_>>();
+	title.push(0);
+	body.push(0);
+	unsafe {
+		MessageBoxW(
+			None,
+			PCWSTR::from_raw(body.as_ptr()),
+			PCWSTR::from_raw(title.as_ptr()),
+			MESSAGEBOX_STYLE(style)
+		).0 as u32
+	}
+}
+
+/// A nicer syntax for [`with_context`].
+macro c($e:expr, $($a:tt)*) {
+	<anyhow::Result<_> as anyhow::Context<_, _>>::with_context(try { $e }, || format!($($a)*))
+}
+
+/// Shows an error in an appropriate way, returning the value as an `Option`.
+fn show_error<T>(a: anyhow::Result<T>) -> Option<T> {
+	match a {
+		Ok(v) => Some(v),
+		Err(e) => {
+			let s = format!("{e:#}");
+			println!("{:?}", e.context("LB-ARK error"));
+			msgbox("LB-ARK error", &s, 0x10);
+			None
+		}
+	}
+}
+
+/// Converts the path to be relative to the game directory, for nicer error messages.
+fn rel(path: &Path) -> &Path {
+	path.strip_prefix(*GAME_DIR).unwrap_or(path)
 }
