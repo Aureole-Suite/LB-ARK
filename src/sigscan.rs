e18027b3d488dd050047ef9309cae750ffc692a2
Kyuuhachi <caagr98@gmail.com>
2023-07-17 16:06:56 +0200

    Sigscan cleanup

--- src/sigscan.rs
+++ src/sigscan.rs
@@ -9,9 +9,8 @@ macro unit {
 
 #[track_caller]
 pub fn scan(sig: &[Option<u8>]) -> *const u8 {
-	let start = 0x00400000;
 	let data: &'static [u8] = unsafe {
-		std::slice::from_raw_parts(start as *const u8, 0x00200000)
+		std::slice::from_raw_parts(0x00400000 as *const u8, 0x00200000)
 	};
 
 	let Some(a) = sig[0] else { panic!() };
@@ -19,7 +18,7 @@ pub fn scan(sig: &[Option<u8>]) -> *const u8 {
 		.find(|&a| data[a..].iter().zip(sig).all(|(a,b)| b.map_or(true, |b| *a==b)))
 		.unwrap();
 
-	(start + offset) as *const u8
+	&data[offset]
 }
 
 pub macro sigscan($($a:tt)*) {
