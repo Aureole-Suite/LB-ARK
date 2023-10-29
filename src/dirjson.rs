11db79d243dd377b28037b84fd981d85a208b142
Kyuuhachi <caagr98@gmail.com>
2023-10-29 13:46:12 +0100

    Replace deserializer with serde_with

--- src/dirjson.rs
+++ src/dirjson.rs
@@ -1,8 +1,15 @@
-use serde::{de::Error, de::Unexpected, Deserialize, Deserializer};
+use std::{marker::PhantomData, str::FromStr};
 
+use serde::{
+	de::{self, Error, Unexpected},
+	Deserialize, Deserializer,
+};
+use serde_with::DeserializeAs;
+
+#[serde_with::serde_as]
 #[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize)]
 pub struct DirJson {
-	#[serde(deserialize_with = "pairs::deserialize")]
+	#[serde_as(as = "serde_with::Map<_, StringOrStruct<_>>")]
 	pub entries: Vec<(Key, Entry)>,
 }
 
@@ -34,81 +41,53 @@ impl<'de> Deserialize<'de> for Key {
 	}
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(serde::Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct Entry {
 	pub name: Option<String>,
 	pub path: String,
 }
 
-#[derive(serde::Deserialize)]
-#[serde(remote = "Entry")]
-struct EntryDef {
-	#[serde(default)]
-	pub name: Option<String>,
-	pub path: String,
-}
+impl FromStr for Entry {
+	type Err = std::convert::Infallible;
 
-impl<'de> Deserialize<'de> for Entry {
-	fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-		struct Vis;
-		impl<'de> serde::de::Visitor<'de> for Vis {
-			type Value = Entry;
-
-			fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-				formatter.write_str("string or map")
-			}
-
-			fn visit_str<E>(self, value: &str) -> Result<Entry, E>
-			where
-				E: serde::de::Error,
-			{
-				Ok(Entry {
-					name: None,
-					path: value.to_owned(),
-				})
-			}
-
-			fn visit_map<M: serde::de::MapAccess<'de>>(self, map: M) -> Result<Entry, M::Error> {
-				EntryDef::deserialize(serde::de::value::MapAccessDeserializer::new(map))
-			}
-		}
-
-		deserializer.deserialize_any(Vis)
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		Ok(Entry {
+			name: None,
+			path: s.to_owned(),
+		})
 	}
 }
 
-mod pairs {
-	use serde::de::{Deserialize, MapAccess, Visitor};
-	use std::marker::PhantomData;
+pub struct StringOrStruct<T>(PhantomData<T>);
 
-	pub fn deserialize<'de, K, V, D: serde::de::Deserializer<'de>>(
-		deserializer: D,
-	) -> Result<Vec<(K, V)>, D::Error>
-	where
-		K: Deserialize<'de>,
-		V: Deserialize<'de>,
-	{
-		struct MyVisitor<K, V>(PhantomData<(K, V)>);
+impl<'de, T, U> DeserializeAs<'de, T> for StringOrStruct<U>
+where
+	StringOrStructV<T, U>: de::Visitor<'de, Value = T>,
+{
+	fn deserialize_as<D: Deserializer<'de>>(de: D) -> Result<T, D::Error> {
+		de.deserialize_any(StringOrStructV::<T, U>(PhantomData))
+	}
+}
 
-		impl<'d, K, V> Visitor<'d> for MyVisitor<K, V>
-		where
-			K: serde::Deserialize<'d>,
-			V: serde::Deserialize<'d>,
-		{
-			type Value = Vec<(K, V)>;
+struct StringOrStructV<T, U>(PhantomData<(T, U)>);
 
-			fn expecting(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-				f.write_str("map")
-			}
+impl<'de, T, U> de::Visitor<'de> for StringOrStructV<T, U>
+where
+	U: DeserializeAs<'de, T>,
+	T: FromStr,
+	T::Err: std::fmt::Display,
+{
+	type Value = T;
 
-			fn visit_map<M: MapAccess<'d>>(self, mut access: M) -> Result<Self::Value, M::Error> {
-				let mut i = Vec::new();
-				while let Some((key, value)) = access.next_entry()? {
-					i.push((key, value))
-				}
-				Ok(i)
-			}
-		}
-		Ok(deserializer.deserialize_map(MyVisitor(PhantomData))?)
+	fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+		formatter.write_str("string or map")
+	}
+
+	fn visit_str<E: de::Error>(self, value: &str) -> Result<T, E> {
+		T::from_str(value).map_err(de::Error::custom)
+	}
+
+	fn visit_map<M: de::MapAccess<'de>>(self, map: M) -> Result<T, M::Error> {
+		U::deserialize_as(de::value::MapAccessDeserializer::new(map))
 	}
 }
